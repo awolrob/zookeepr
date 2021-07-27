f5467be1074ffd773b5a1077ecd423240d239508
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-07-26 21:40:34 -0400

    11.2

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -52,7 +50,23 @@ function createNewAnimal(body, animalsArray) {
         JSON.stringify({ animals: animalsArray }, null, 2)
     );
     return animal;
-};
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -71,34 +85,10 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-
-    return animal;
-}
-
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-};
-
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
@@ -106,7 +96,6 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
